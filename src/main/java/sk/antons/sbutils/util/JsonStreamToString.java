413076c72aa12bc9617073252d7e96bfbef9b2ba
Anton Straka <anton.straka@gmail.com>
2023-10-31 08:21:06 +0100

    interceptor reimplementation

---
+++ src/main/java/sk/antons/sbutils/util/JsonStreamToString.java
@@ -0,0 +1,69 @@
+/*
+ *
+ */
+package sk.antons.sbutils.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import sk.antons.jaul.xml.XmlFormat;
+import sk.antons.json.util.JsonFormat;
+
+
+/**
+ *
+ * @author antons
+ */
+public class JsonStreamToString {
+    String encoding = "utf-8";
+    boolean formated;
+    boolean forceOneLine;
+    String indent;
+    int cufStringLiteralsLength;
+    public static JsonStreamToString instance() { return new JsonStreamToString(); }
+    public JsonStreamToString encoding(String value) { this.encoding = value; return this; }
+    public JsonStreamToString forceOneLine() { this.forceOneLine = true; this.formated = true; return this; }
+    public JsonStreamToString indent(String value) { this.indent = value; this.formated = true; return this; }
+    public JsonStreamToString cufStringLiterals(int value) { this.cufStringLiteralsLength = value; this.formated = true; return this; }
+
+    public Function<InputStream, String> transform() {
+        if(formated) return formatted();
+        else return  asIs();
+    }
+
+    private Function<InputStream, String> asIs() {
+        return is -> {
+            return readStream(is, encoding);
+        };
+    }
+
+    private Function<InputStream, String> formatted() {
+        return is -> {
+            try {
+                JsonFormat format = JsonFormat.from(new InputStreamReader(is, encoding));
+                if(cufStringLiteralsLength > 0) format.cutStringLiterals(cufStringLiteralsLength);
+                if(indent != null) format.indent(indent);
+                else if(forceOneLine) format.noindent();
+                return format.toText();
+            } catch(Exception e) {
+                throw new IllegalArgumentException();
+            }
+        };
+    }
+
+    private static String readStream(InputStream is, String encoding) {
+        if(is == null) return null;
+        try {
+            InputStreamReader isr = new InputStreamReader(is, encoding);
+            String s = new BufferedReader(isr).lines()
+                .collect(Collectors.joining("\n"));
+            return s;
+        } catch(Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+}
+
--- src/main/java/sk/antons/sbutils/ws/SBWSLoggingInterceptor.java
+++ src/main/java/sk/antons/sbutils/ws/SBWSLoggingInterceptor.java
@@ -1,30 +1,37 @@
 package sk.antons.sbutils.ws;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 import org.springframework.ws.WebServiceMessage;
 import org.springframework.ws.client.WebServiceIOException;
 import org.springframework.ws.client.support.interceptor.ClientInterceptor;
 import org.springframework.ws.context.MessageContext;
-import sk.antons.jaul.xml.XmlFormat;
+import sk.antons.sbutils.util.XmlStreamToString;
 
 
 public class SBWSLoggingInterceptor implements ClientInterceptor {
 
     private Consumer<String> logger = null;
     private BooleanSupplier loggerEnabled = null;
-    private boolean forceOneLine = false;
+    private Function<InputStream, String> format;
 
     private SBWSLoggingInterceptor() {}
     public static SBWSLoggingInterceptor instance() { return new SBWSLoggingInterceptor(); }
 
     public SBWSLoggingInterceptor logger(Consumer<String> value) { this.logger = value; return this; }
     public SBWSLoggingInterceptor loggerEnabled(BooleanSupplier value) { this.loggerEnabled = value; return this; }
-    public SBWSLoggingInterceptor forceOneLine(boolean value) { this.forceOneLine = value; return this; }
+    public SBWSLoggingInterceptor format(Function<InputStream, String> value) { this.format = value; return this; }
+
+    public static class Format {
+        public static XmlStreamToString xml() { return XmlStreamToString.instance(); }
+    }
 
     @Override
     public boolean handleRequest(MessageContext messageContext) {
@@ -53,8 +60,7 @@ public class SBWSLoggingInterceptor implements ClientInterceptor {
         if (loggerEnabled.getAsBoolean()) {
             try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
                 message.writeTo(stream);
-                String xml = stream.toString(StandardCharsets.UTF_8);
-                if(forceOneLine) xml = XmlFormat.instance(xml, 0).forceoneline().format();
+                String xml = format == null ? stream.toString(StandardCharsets.UTF_8) : format.apply(new ByteArrayInputStream(stream.toByteArray()));
                 logger.accept(messageType + ": " + xml);
             } catch (IOException e) {
                 throw new WebServiceIOException("Error logging " + messageType, e);
